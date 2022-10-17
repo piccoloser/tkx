8597ea34ebdd8cc760660c790b8b5cfa697de3f8
Hunter Logan <hunter.gray.logan@gmail.com>
2022-10-17 13:41:27 -0400

    Implement styling for nested elements

--- tks/element.py
+++ tks/element.py
@@ -1,3 +1,7 @@
+from __future__ import annotations
+from tks.dbg import dbg
+from tks.stylesheet import Stylesheet
+from typing import Optional
 import tkinter as tk
 
 
@@ -10,12 +14,56 @@ class Element:
     ):
         self.id = kwargs.pop("id", None)
         self.cl = kwargs.pop("cl", None)
-        self.widget = widget(parent, **kwargs)
 
-        if parent.__dict__.get("elements") is not None:
-            if parent.stylesheet is not None:
-                if parent.stylesheet.get(widget.__name__):
-                    self.widget.configure(**parent.stylesheet.get(widget.__name__))
+        self.elements = None
+        self.parent = parent
 
+        if isinstance(parent, Element):
+            self.widget = widget(self.parent.widget, **kwargs)
+        else:
+            self.widget = widget(self.parent, **kwargs)
+
+        fallback: Optional[str] = None
+        match widget.__name__:
+            case "Frame":
+                fallback = "Window"
+
+        self.configure(**self.get_style(widget.__name__, fallback))
+
+    @dbg
+    def add(self, widget: tk.Widget, **kwargs) -> Element:
+        """Add a new child Element to this Element."""
+        if self.elements is None:
+            self.elements = []
+
+        element = Element(widget, self, **kwargs)
+        self.elements.append(element)
+        element.widget.pack()
+        return element
+
+    @dbg
     def bind(self, *args):
         self.widget.bind(*args)
+
+    @dbg
+    def configure(self, **kwargs):
+        for p in ("cl", "id"):
+            if kwargs.get(p, None) is not None:
+                self.__dict__[p] = kwargs[p]
+
+        if kwargs:
+            self.widget.configure(**kwargs)
+
+    @dbg
+    def get_style(
+        self, name: str, fallback: Optional[str] = None
+    ) -> Optional[Stylesheet]:
+        return self.parent.get_style(name) or self.parent.get_style(fallback)
+
+    @property
+    def parent(self) -> tk.Widget or Element:
+        return self.__parent
+
+    @parent.setter
+    def parent(self, item: tk.Widget or Element) -> None:
+        self.__parent = item
