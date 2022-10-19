67888a7d95b175191481ab47cf0f684ab0872362
Hunter Logan <hunter.gray.logan@gmail.com>
2022-10-18 20:31:15 -0400

    Fix package info

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from distutils.core import setup
 
 setup(
     name="tks",
-    version="1.0",
+    version="0.1.0",
     description="tkinter superset",
     author="Hunter Logan",
     author_email="_@piccoloser.com",
